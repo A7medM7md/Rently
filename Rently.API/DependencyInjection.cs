9739b6102a9fe8778f217e662384ae656b96328e
AhmedM7med <ahmedelgebaly2511@gmail.com>
2025-08-16 23:23:26 +0300

    create separate dependancy files and link layers

---
+++ Rently.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Rently.Application;
+using Rently.Infrastructure;
+
+namespace Rently.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
---
+++ Rently.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rently.Application
+{
+    // Create Separate Dependancy File 
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
